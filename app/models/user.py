46eb62705c72f7ca0a48e6bc8a9a74c3f0d76991
Lukas Günther <lukas.guenther@tib.eu>
2022-11-06 22:27:19 +0100

    added codestyle

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,13 @@
+"""User Class"""
+from sqlalchemy import Column, Integer, String
+
 from ..db import database
-from sqlalchemy import Column, ForeignKey, Boolean, Integer, String
-from sqlalchemy.orm import relationship
+
 
 class User(database.Base):
-    __tablename__   = 'users'
-    
-    id              = Column(Integer, primary_key=True, index=True)
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
     hashed_password = Column(String)
-    email           = Column(String, unique=True, index=True)
-    name            = Column(String)
+    email = Column(String, unique=True, index=True)
+    name = Column(String)
---
+++ app/schemas/user_schemas.py
@@ -0,0 +1,23 @@
+"""Schemas for User"""
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    """Pydantic Base Class for User"""
+    email: str
+
+
+class UserCreate(UserBase):
+    """Pydantic Class for User Creation"""
+    password: str
+    name: str
+ 
+
+class User(UserBase):
+    """Pydantic Class for User"""
+    id: int
+    name: str
+
+    class Config:
+        """Pydantic Config Class for User"""
+        orm_mode = True
