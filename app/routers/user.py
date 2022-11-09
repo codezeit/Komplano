9f920f4508cc2c80bfb75805f98af7dbcf492fef
Lukas Günther <lukas.guenther@tib.eu>
2022-11-09 22:28:26 +0100

    removed unused method 🕳️

--- app/routers/user.py
+++ app/routers/user.py
@@ -34,15 +34,3 @@ def read_users(
     """"Returns a list of users."""
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
-
-
-@router.get("/{user_id}", response_model=user_schemas.User)
-def read_user(
-        user_id: int,
-        db: Session = Depends(get_db)
-        ):
-    """"Returns a single user."""
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
