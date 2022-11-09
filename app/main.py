2c9a9db600576bae1b4b0468410ce3aa00c7efa8
Lukas Günther <lukas.guenther@tib.eu>
2022-11-09 21:43:51 +0100

    removed linter errors 🧨

--- app/main.py
+++ app/main.py
@@ -2,13 +2,17 @@
 
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from .db.database import SessionLocal, Base, get_engine
+from .db.database import Base, get_engine
 from .routers import user, auth
 
 load_dotenv()
 
 Base.metadata.create_all(bind=get_engine())
 
-app = FastAPI()
+app = FastAPI(
+    title="Komplano API",
+    description="The API for the Komplano Chore Management App",
+    version="0.1.0",
+)
 app.include_router(user.router)
 app.include_router(auth.router)
--- app/services/auth.py
+++ app/services/auth.py
@@ -1,28 +1,35 @@
 """Authentication Service layer"""
 from datetime import datetime, timedelta
-
-from ..config import get_settings
 from passlib.context import CryptContext
-from jose import JWTError, jwt
+from jose import jwt
+from ..config import get_settings
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 settings = get_settings()
 
+
 def verify_password(plain_password, hashed_password):
+    """Verify a password against a hashed password"""
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password):
+    """Hash a password"""
     return pwd_context.hash(password)
 
 
 def create_access_token(data: dict):
+    """Create an access token with JWT"""
     to_encode = data.copy()
     if settings.access_token_expire_minutes:
-        expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
+        expire = datetime.utcnow() + \
+            timedelta(minutes=settings.access_token_expire_minutes)
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.secret_key,
+        algorithm=settings.algorithm)
     return encoded_jwt
