b4dd5637199f69bf782fe0e133c33c0b8db85f47
Lukas Günther <lukas.guenther@tib.eu>
2022-11-11 09:59:43 +0100

    added CORS settings 🦹

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 """Main Module"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from .db.database import Base, get_engine
 from .routers import user, auth
@@ -14,5 +15,20 @@ app = FastAPI(
     description="The API for the Komplano Chore Management App",
     version="0.1.0",
 )
+
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.router)
 app.include_router(auth.router)
